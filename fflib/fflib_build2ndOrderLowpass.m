e8a88d8f1bd408810cbbeb9d4d6d6fcbbdf52ca3
Jan <j.bolting@gmx.de>
2016-07-20 20:42:22 +0200

    Deleted some commented obsolete code.

--- fflib/fflib_build2ndOrderLowpass.m
+++ fflib/fflib_build2ndOrderLowpass.m
@@ -3,8 +3,6 @@
 % \author Jan Bolting, ONERA/ISAE, 2014, j.bolting@isae.fr
 %
 function tfct = fflib_build2ndOrderLowpass(omega_n, damping, dcgain)
-% num = dcgain * omega_n^2;
-% den = [1 2*damping*omega_n omega_n^2];
 num = dcgain * omega_n^2;
 den = [1 2*damping*omega_n omega_n^2];
 
